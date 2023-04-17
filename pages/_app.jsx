4d5582f30542f7ad2ce8e43eeefa6346e2ea99e3
Emanuele Colabello <emanuele.colabello@gmail.com>
2023-04-17 19:34:59 +0200

    Rename _app.js into _app.jsx.

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,4 +1,4 @@
-import '@/styles/globals.css';
+import '@/styles/globals.css'; // eslint-disable-line import/no-unresolved
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
